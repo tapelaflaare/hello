3cdfd225f9fc757b0c4a732cd924b96cc4177b25
Murat Orazkhanov <holdonmurat@gmail.com>
2025-01-28 10:44:47 +0600

    Add files via upload

--- main.go
+++ main.go
@@ -5,14 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 	var name string
-	name = "mura"
-
+	name = " MMM"
 	fmt.Println(name)
 
-	var num float32
-	num = 12
-
-	var num2 float32
-	num2 = 32.5
-	fmt.Println(num + num)
 }
