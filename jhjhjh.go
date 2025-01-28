950064d6d17d787d4656cb37ccbcf5d19814426a
Murat Orazkhanov <holdonmurat@gmail.com>
2025-01-28 10:44:24 +0600

    Add files via upload

---
+++ jhjhjh.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	var name string
+	name = " MMM"
+	fmt.Println(name)
+
+	var num float32 
+    num = 12;
+
+ 	var num2 float32 
+    num = 12;
+	
+
+	fmt.Println(num2 + num) 
+}
